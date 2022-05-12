e14a55ff2df3fc5cc6d2a1f9563d786e629a1454
vera chuang <53413276+veraachuang@users.noreply.github.com>
2022-05-13 04:14:58 +0800

    Delete kommunicate_flutter_plugin_example directory

--- kommunicate_flutter_plugin_example/ios/Classes/KommunicateFlutterPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface KommunicateFlutterPlugin : NSObject<FlutterPlugin>
-@end
--- kommunicate_flutter_plugin_example/ios/Classes/KommunicateFlutterPlugin.m
+++
@@ -1,8 +0,0 @@
-#import "KommunicateFlutterPlugin.h"
-#import <kommunicate_flutter/kommunicate_flutter-Swift.h>
-
-@implementation KommunicateFlutterPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftKommunicateFlutterPlugin registerWithRegistrar:registrar];
-}
-@end
